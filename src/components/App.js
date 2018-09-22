2ef291443579f5b051094931fb75aa0fd9ebda4e
Xarri Jorge <kodinglife@gmail.com>
2018-09-22 13:31:41 +0000

    completed login/out functions. added function to persist login until logout

--- src/components/App.js
+++ src/components/App.js
@@ -22,7 +22,7 @@ class App extends Component {
     const itemsRef = firebase.database().ref('items');
     const item = {
       food: this.state.currentItem,
-      person: this.state.username,
+      person: this.state.user.displayName || this.state.user.email,
     };
     itemsRef.push(item);
     this.setState({
@@ -41,7 +41,20 @@ class App extends Component {
     });
   };
 
+  logout = () => {
+    auth.signOut().then(() => {
+      this.setState({
+        user: null,
+      });
+    });
+  };
+
   componentDidMount() {
+    auth.onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ user });
+      }
+    });
     const itemsRef = firebase.database().ref('items');
     itemsRef.on('value', snapshot => {
       let items = snapshot.val();
@@ -74,50 +87,62 @@ class App extends Component {
             )}
           </div>
         </header>
-        <div className="container">
-          <section className="add-item">
-            <form onSubmit={this.handleSubmit}>
-              <input
-                type="text"
-                required
-                name="username"
-                placeholder="What's your name?"
-                value={this.state.username}
-                onChange={this.handleChange}
-              />
-              <input
-                type="text"
-                required
-                name="currentItem"
-                placeholder="What are you bringing?"
-                value={this.state.currentItem}
-                onChange={this.handleChange}
-              />
-              <button>Add Item</button>
-            </form>
-          </section>
-          <section className="display-item">
-            <div className="wrapper">
-              <ul>
-                {this.state.items.map(item => {
-                  return (
-                    <li key={item.id}>
-                      <h3 className="item-head">
-                        {item.food}{' '}
-                        <i
-                          onClick={() => this.removeItem(item.id)}
-                          className="fa fa-times-circle-o close"
-                          aria-hidden="true"
-                        />
-                      </h3>
-                      <p>brought by: {item.person}</p>
-                    </li>
-                  );
-                })}
-              </ul>
+        {this.state.user ? (
+          <div className="container">
+            <div className="user-profile">
+              <img src={this.state.user.photoURL} />
             </div>
-          </section>
-        </div>
+            <section className="add-item">
+              <form onSubmit={this.handleSubmit}>
+                <input
+                  type="text"
+                  required
+                  name="username"
+                  placeholder="What's your name?"
+                  value={this.state.user.displayName || this.state.user.email}
+                  onChange={this.handleChange}
+                />
+                <input
+                  type="text"
+                  required
+                  name="currentItem"
+                  placeholder="What are you bringing?"
+                  value={this.state.currentItem}
+                  onChange={this.handleChange}
+                />
+                <button>Add Item</button>
+              </form>
+            </section>
+            <section className="display-item">
+              <div className="wrapper">
+                <ul>
+                  {this.state.items.map(item => {
+                    return (
+                      <li key={item.id}>
+                        <h3 className="item-head">
+                          {item.food}
+                          {item.person === this.state.user.displayName ||
+                          item.person === this.state.user.email ? (
+                            <i
+                              onClick={() => this.removeItem(item.id)}
+                              className="fa fa-times-circle-o close"
+                              aria-hidden="true"
+                            />
+                          ) : null}
+                        </h3>
+                        <p>brought by: {item.person}</p>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            </section>
+          </div>
+        ) : (
+          <div className="wrapper login-message">
+            <h2>Please log in to see the potluck list and submit to it.</h2>
+          </div>
+        )}
       </div>
     );
   }
